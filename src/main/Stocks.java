be7db6e58d6805a71b1c959ecc81671ffdbd8456
Francine Burling-Claridge <francinebc.mail@gmail.com>
2019-03-26 11:12:31 +1300

    first commit

---
+++ src/main/Stocks.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Stocks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int getMaxProfit(int[] stockPrices) {
+		return 6;
+	}
+
+}
